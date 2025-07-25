c6291a5ae79c2596ca4dc00ca81aa2591aee9d9e
chandrasas <45199099+chandrasas@users.noreply.github.com>
2025-07-25 15:56:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcomce to AWS CI/CD Demo'));
+app.get('/', (req, res) => res.send('Welcomce You All to AWS CI/CD Demo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
